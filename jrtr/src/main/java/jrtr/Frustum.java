d27a7a4df9980a6df16637dd4743b3e1c3b3bd79
Orlando Signer <orlando.signer@students.unibe.ch>
2014-10-03 21:36:41 +0200

    frustum impl

--- jrtr/src/main/java/jrtr/Frustum.java
+++ jrtr/src/main/java/jrtr/Frustum.java
@@ -3,40 +3,88 @@ package jrtr;
 import javax.vecmath.Matrix4f;
 
 /**
- * Stores the specification of a viewing frustum, or a viewing
- * volume. The viewing frustum is represented by a 4x4 projection
- * matrix. You will extend this class to construct the projection 
- * matrix from intuitive parameters.
+ * Stores the specification of a viewing frustum, or a viewing volume. The
+ * viewing frustum is represented by a 4x4 projection matrix. You will extend
+ * this class to construct the projection matrix from intuitive parameters.
  * <p>
- * A scene manager (see {@link SceneManagerInterface}, {@link SimpleSceneManager}) 
- * stores a frustum.
+ * A scene manager (see {@link SceneManagerInterface},
+ * {@link SimpleSceneManager}) stores a frustum.
  */
 public class Frustum {
 
-	private Matrix4f projectionMatrix;
-	
-	/**
-	 * Construct a default viewing frustum. The frustum is given by a 
-	 * default 4x4 projection matrix.
-	 */
-	public Frustum()
-	{
-		projectionMatrix = new Matrix4f();
-		float f[] = {2.f, 0.f, 0.f, 0.f, 
-					 0.f, 2.f, 0.f, 0.f,
-				     0.f, 0.f, -1.02f, -2.02f,
-				     0.f, 0.f, -1.f, 0.f};
-		projectionMatrix.set(f);
-	}
-	
-	/**
-	 * Return the 4x4 projection matrix, which is used for example by 
-	 * the renderer.
-	 * 
-	 * @return the 4x4 projection matrix
-	 */
-	public Matrix4f getProjectionMatrix()
-	{
-		return projectionMatrix;
-	}
+    private Matrix4f projectionMatrix;
+    private float fov;
+    private float aspect;
+    private float near;
+    private float far;
+
+    /**
+     * Construct a default viewing frustum. The frustum is given by a default
+     * 4x4 projection matrix.
+     */
+    public Frustum() {
+        fov = 90;
+        aspect = 0.5F;
+        near = 0F;
+        far = 50;
+        projectionMatrix = new Matrix4f();
+        update();
+    }
+
+    /**
+     * Return the 4x4 projection matrix, which is used for example by the
+     * renderer.
+     *
+     * @return the 4x4 projection matrix
+     */
+    public Matrix4f getProjectionMatrix() {
+        return projectionMatrix;
+    }
+
+    public float getFov() {
+        return fov;
+    }
+
+    public void setFov(float fov) {
+        this.fov = fov;
+        update();
+    }
+
+    public float getAspect() {
+        return aspect;
+    }
+
+    public void setAspect(float aspect) {
+        this.aspect = aspect;
+        update();
+    }
+
+    public float getNear() {
+        return near;
+    }
+
+    public void setNear(float near) {
+        this.near = near;
+        update();
+    }
+
+    public float getFar() {
+        return far;
+    }
+
+    public void setFar(float far) {
+        this.far = far;
+        update();
+    }
+
+    private void update() {
+        // lecture 3 page 52
+        projectionMatrix.setIdentity();
+        projectionMatrix.m00 = (float) (1 / (aspect * Math.tan(fov / 2)));
+        projectionMatrix.m11 = (float) (1 / Math.tan(fov / 2));
+        projectionMatrix.m22 = (near + far) / (near - far);
+        projectionMatrix.m23 = (2 * near * far) / (near - far);
+        projectionMatrix.m32 = -1;
+    }
+
 }
