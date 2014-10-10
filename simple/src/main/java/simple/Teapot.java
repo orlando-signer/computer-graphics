8241215a2c63d1e03c8fd6b41014f44e8c56cfe9
Orlando Signer <orlando.signer@students.unibe.ch>
2014-10-10 15:16:06 +0200

    coloring for teapot added

--- simple/src/main/java/simple/Teapot.java
+++ simple/src/main/java/simple/Teapot.java
@@ -1,7 +1,11 @@
 package simple;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import util.Color;
+import util.Utils;
 import jrtr.ObjReader;
 import jrtr.RenderContext;
 import jrtr.Shape;
@@ -16,7 +20,13 @@ public class Teapot implements Model {
     @Override
     public Shape createShape(RenderContext ctx) {
         try {
-            VertexData data = ObjReader.read("../obj/teapot.obj", 5, ctx);
+            VertexData data = ObjReader.read("../obj/teapot.obj", 20F, ctx);
+           
+            List<Color> colors = new ArrayList<>(data.getNumberOfVertices());
+            for(int i = 0;i<data.getNumberOfVertices();i++)
+                colors.add(Color.WHITE);
+            data.addElement(Utils.colorToArray(colors), VertexData.Semantic.COLOR, 3);
+
             return new Shape(data);
         } catch (IOException e) {
             throw new IllegalStateException("could not load teapot from obj-file", e);
