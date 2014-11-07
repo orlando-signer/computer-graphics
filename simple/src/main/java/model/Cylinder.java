21202a3607314c7af1f49c4a2cb557961b19dd6b
Orlando Signer <orlando.signer@students.unibe.ch>
2014-11-07 17:04:12 +0100

    add texture coords for cylinder

--- simple/src/main/java/model/Cylinder.java
+++ simple/src/main/java/model/Cylinder.java
@@ -3,6 +3,7 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.vecmath.Point2f;
 import javax.vecmath.Point3d;
 import javax.vecmath.Vector3d;
 
@@ -17,7 +18,7 @@ import com.google.common.primitives.Ints;
  * @author Orlando Signer
  *
  */
-public class Cylinder implements Model{
+public class Cylinder implements Model {
     private final int segments;
     private final float radius;
     private final float height;
@@ -37,6 +38,8 @@ public class Cylinder implements Model{
         int segmentsTimesTwo = segments * 2;
         List<Point3d> vertices = new ArrayList<>(segmentsTimesTwo + 2);
         List<Vector3d> normals = new ArrayList<>(segmentsTimesTwo + 2);
+        List<Point2f> textures = new ArrayList<>(segmentsTimesTwo);
+
         for (int i = 0; i < segments; i++) {
             tmpX = (float) Math.sin(i * angle) * radius;
             tmpZ = (float) Math.cos(i * angle) * radius;
@@ -46,13 +49,17 @@ public class Cylinder implements Model{
             normal.normalize();
             normals.add(normal);
             normals.add(normal);
+            textures.add(new Point2f((float) i / segments, 0));
+            textures.add(new Point2f((float) i / segments, -1));
 
         }
         // Add bottom and top
         vertices.add(new Point3d(0, height / -2, 0));
         vertices.add(new Point3d(0, height / 2, 0));
-        normals.add(new Vector3d(0,-1,0));
-        normals.add(new Vector3d(0,1,0));
+        normals.add(new Vector3d(0, -1, 0));
+        normals.add(new Vector3d(0, 1, 0));
+        textures.add(new Point2f(0.5F, 0.5F));
+        textures.add(new Point2f(0.5F, 0.5F));
 
         List<Integer> indices = new ArrayList<>(segments * 3 * 3);
         for (int i = 0; i < segmentsTimesTwo; i++) {
@@ -61,7 +68,7 @@ public class Cylinder implements Model{
             indices.add((i + 2) % segmentsTimesTwo);
         }
 
-        // indices for top and bottom
+        // indices for top and bottobm
         for (int i = 0; i < segmentsTimesTwo; i += 2) {
             indices.add(i);
             indices.add((i + 2) % segmentsTimesTwo);
@@ -86,26 +93,13 @@ public class Cylinder implements Model{
         colors.add(Color.WHITE);
         colors.add(Color.WHITE);
 
-        // The vertex normals
-        // TODO vertex normals and texture coordinates don't matter for
-        // assignement 1
-
-        // Texture coordinates
-        float uv[] = { 0, 0, 1, 0, 1, 1, 0, 1, //
-                0, 0, 1, 0, 1, 1, 0, 1, //
-                0, 0, 1, 0, 1, 1, 0, 1, //
-                0, 0, 1, 0, 1, 1, 0, 1, //
-                0, 0, 1, 0, 1, 1, 0, 1, //
-                0, 0, 1, 0, 1, 1, 0, 1 };
-
         // Construct a data structure that stores the vertices, their
         // attributes, and the triangle mesh connectivity
         VertexData vertexData = ctx.makeVertexData(segmentsTimesTwo + 2);
         vertexData.addElement(Utils.tuple3dToArray(vertices), VertexData.Semantic.POSITION, 3);
         vertexData.addElement(Utils.colorToArray(colors), VertexData.Semantic.COLOR, 3);
         vertexData.addElement(Utils.tuple3dToArray(normals), VertexData.Semantic.NORMAL, 3);
-        // vertexData.addElement(n, VertexData.Semantic.NORMAL, 3);
-        // vertexData.addElement(uv, VertexData.Semantic.TEXCOORD, 2);
+        vertexData.addElement(Utils.points2fToArray(textures), VertexData.Semantic.TEXCOORD, 2);
         vertexData.addIndices(Ints.toArray(indices));
 
         return new jrtr.Shape(vertexData);
