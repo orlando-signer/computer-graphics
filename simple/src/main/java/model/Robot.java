94840eb70ec208ce52003a2db27680b6f21cfc9c
Orlando Signer <orlando.signer@students.unibe.ch>
2014-11-18 16:37:03 +0100

    legs impl, animation not working corectly

--- simple/src/main/java/model/Robot.java
+++ simple/src/main/java/model/Robot.java
@@ -13,9 +13,11 @@ public class Robot {
     private TransformGroup root;
 
     private float rotate;
-    private TransformGroup arm;
+    private TransformGroup arms;
     private TransformGroup head;
     private TransformGroup body;
+    private TransformGroup leftLeg;
+    private TransformGroup rightLeg;
 
     public Robot(TransformGroup root, RenderContext ctx) {
         this.root = root;
@@ -25,7 +27,7 @@ public class Robot {
 
     private void init() {
         Matrix4f m = getIdentity();
-        m.m03 = 5;
+        // m.m03 = 5;
         body = new TransformGroup("BodyGroup");
         body.setTransformation(m);
         root.addChild(body);
@@ -41,28 +43,55 @@ public class Robot {
         head.addChild(new ShapeNode(new Cylinder(10, 0.1F, 1F).createShape(ctx), mAntenna, "AntennaNode"));
         body.addChild(head);
 
-        arm = new TransformGroup("ArmGroup");
+        // Arms
+        arms = new TransformGroup("ArmGroup");
         Shape armShape = new Cylinder(10, 0.8F, 2).createShape(ctx);
         Shape lowerArmShape = new Cylinder(10, 0.6F, 1.5F).createShape(ctx);
         Matrix4f mLeftArm = getIdentity();
         mLeftArm.m03 = 2.4F;
         Matrix4f mLowerLeftArm = getIdentity();
-        mLowerLeftArm.rotX((float) (30 * 180 / Math.PI));
+        mLowerLeftArm.rotX((float) (30F / 180F * Math.PI));
         mLowerLeftArm.m03 = 2.5F;
-        mLowerLeftArm.m13 = 3.3F;
-        mLowerLeftArm.m23 = 0.6F;
-        arm.addChild(new ShapeNode(armShape, mLeftArm, "LeftArmNode"));
-        arm.addChild(new ShapeNode(lowerArmShape, mLowerLeftArm, "LowerLeftArmNode"));
-        arm.getTransformation().m13 = 3.5F;
+        mLowerLeftArm.m13 = 2F;
+        // mLowerLeftArm.m23 = 0.6F;
+        arms.addChild(new ShapeNode(armShape, mLeftArm, "LeftArmNode"));
+        arms.addChild(new ShapeNode(lowerArmShape, mLowerLeftArm, "LowerLeftArmNode"));
 
         Matrix4f mRightArm = new Matrix4f(mLeftArm);
         mRightArm.m03 = -2.5F;
         Matrix4f mLowerRightArm = new Matrix4f(mLowerLeftArm);
         mLowerRightArm.m03 = -2.4F;
-        arm.addChild(new ShapeNode(armShape, mRightArm, "RightArmNode"));
-        arm.addChild(new ShapeNode(lowerArmShape, mLowerRightArm, "LowerRightArmNode"));
+        arms.addChild(new ShapeNode(armShape, mRightArm, "RightArmNode"));
+        arms.addChild(new ShapeNode(lowerArmShape, mLowerRightArm, "LowerRightArmNode"));
 
-        body.addChild(arm);
+        arms.getTransformation().m13 = 3.5F;
+        body.addChild(arms);
+
+        // Legs
+        leftLeg = new TransformGroup("LeftLegGroup");
+        Shape legShape = new Cylinder(10, 0.8F, 4).createShape(ctx);
+        Shape footShape = new Cylinder(10, 0.5F, 1.5F).createShape(ctx);
+        Matrix4f mLeftLeg = getIdentity();
+        mLeftLeg.m03 = -0.9F;
+        Matrix4f mLeftFoot = getIdentity();
+        mLeftFoot.rotX((float) (90F / 180F * Math.PI));
+        mLeftFoot.m03 = -0.8F;
+        mLeftFoot.m13 = 0.25F;
+        mLeftFoot.m23 = -2F;
+        leftLeg.addChild(new ShapeNode(legShape, mLeftLeg, "LeftLegNode"));
+        leftLeg.addChild(new ShapeNode(footShape, mLeftFoot, "LeftFootNode"));
+        leftLeg.getTransformation().m13 = -4F;
+        body.addChild(leftLeg);
+
+        rightLeg = new TransformGroup("RightLegGroup");
+        Matrix4f mRightLeg = getIdentity();
+        mRightLeg.m03 = 0.9F;
+        Matrix4f mRightFoot = new Matrix4f(mLeftFoot);
+        mRightFoot.m03 = 0.8F;
+        rightLeg.addChild(new ShapeNode(legShape, mRightLeg, "RightLegNode"));
+        rightLeg.addChild(new ShapeNode(footShape, mRightFoot, "RightFootNode"));
+        rightLeg.getTransformation().m13 = -4F;
+        body.addChild(rightLeg);
     }
 
     public Node getTransformGroup() {
@@ -72,12 +101,17 @@ public class Robot {
     public void animate(float currentstep) {
         Matrix4f m = root.getTransformation();
         rotate += (currentstep / 10);
-        m.rotY(rotate);
+        // m.rotY(rotate);
 
-        m = arm.getTransformation();
-        arm.getTransformation().m13 -= 3.5;
+        m = arms.getTransformation();
+        m.m13 -= 3.5;
         m.rotX(rotate * 10);
-        arm.getTransformation().m13 += 3.5;
+        m.m13 += 3.5;
+
+        // m = rightLeg.getTransformation();
+        // // m.m13 += 4;
+        // m.rotX(rotate * 5);
+        // m.m13 -= 4;
     }
 
     private Matrix4f getIdentity() {
